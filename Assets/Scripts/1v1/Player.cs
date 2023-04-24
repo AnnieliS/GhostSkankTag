c81c099edab1528666997e98269ee3621d94be8a
Annieli Siegel <40207237+AnnieliS@users.noreply.github.com>
2023-04-24 05:38:50 +0300

    fix camera position + control camera with mouse

--- Assets/Scripts/1v1/Player.cs
+++ Assets/Scripts/1v1/Player.cs
@@ -5,29 +5,56 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    [SerializeField] GameObject playerCamera;
     [SerializeField] float speed = 10.0f;
-    [SerializeField] float rotationSpeed = 100.0f;
+    [SerializeField] float rotationMoveSpeed = 100.0f;
     [SerializeField] float currentSpeed = 0;
+    [SerializeField] float rotateCameraSpeed;
     Animator anim;
+    Transform cameraTransform;
     bool isRunning = false;
+
+    float xCamera;
+    float yCamera;
+    float xRotate = 0f;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        cameraTransform = playerCamera.GetComponent<Transform>();
+        //hide mouse cursor
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        CameraControl();
         MoveCharacter();
         CheckSpeed();
 
     }
 
+    private void CameraControl()
+    {
+        // get mouse movement
+        xCamera = Input.GetAxis("Mouse X") * rotateCameraSpeed * Time.deltaTime;
+        yCamera = Input.GetAxis("Mouse Y") * rotateCameraSpeed * Time.deltaTime;
+
+        // assign and lock x rotation
+        xRotate -= yCamera;
+        xRotate = Mathf.Clamp(xRotate, 0f, 30f);
+
+        // actually move some shit
+        cameraTransform.localRotation = Quaternion.Euler(xRotate, 0f, 0f);
+        // cameraTransform.Rotate(Vector3.right* transform.rotation.y);
+        transform.Rotate(Vector3.up * xCamera);
+    }
+
     private void MoveCharacter()
     {
         float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        // float rotation = Input.GetAxis("Horizontal") * rotationMoveSpeed;
 
         translation *= Time.deltaTime;
         currentSpeed *= Time.deltaTime;
@@ -35,7 +62,7 @@ public class Player : MonoBehaviour
         transform.Translate(0, 0, translation);
         currentSpeed = translation;
 
-        transform.Rotate(0, rotation, 0);
+        // transform.Rotate(0, rotation, 0);
     }
 
     private void CheckSpeed()
